f43b71a683731564a2848afc0be567e73b9a9de1
fshi <fshi@FSHI-LT1>
2018-03-20 18:02:02 +0800

    v0.1

--- QuoteServer.cs
+++ QuoteServer.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
 using ExcelDna.Integration;
 using ExcelDna.Integration.Rtd;
+using ExcelDna.Logging;
 
 namespace QuoteRtd
 {
@@ -13,9 +17,10 @@ namespace QuoteRtd
     [ComVisible(true)]
     public class QuoteServer : IRtdServer
     {
-        private Dictionary<int, Topic> _topics;
-        private IRTDUpdateEvent _callback;
-        private System.Windows.Forms.Timer _timer;
+        private Dictionary<int, Topic> m_topics;
+        private IRTDUpdateEvent m_callback;
+        private Timer m_timer;
+        private WebClient m_webClient;
 
         #region IRtdServer Members
 
@@ -42,14 +47,14 @@ namespace QuoteRtd
                 throw new Exception("Invalid item");
             }
 
-            _topics.Add(topicId, new Topic(id, item));
+            m_topics.Add(topicId, new Topic(id, item));
 
             return "Queued";
         }
 
         public void DisconnectData(int topicID)
         {
-            _topics.Remove(topicID);
+            m_topics.Remove(topicID);
         }
 
         public int Heartbeat()
@@ -59,15 +64,15 @@ namespace QuoteRtd
 
         public Array RefreshData(ref int TopicCount)
         {
-            object[,] results = new object[2, _topics.Count];
+            object[,] results = new object[2, m_topics.Count];
             TopicCount = 0;
 
-            foreach (int topicID in _topics.Keys)
+            foreach (int topicID in m_topics.Keys)
             {
-                if (_topics[topicID].Updated == true)
+                if (m_topics[topicID].Updated == true)
                 {
                     results[0, TopicCount] = topicID;
-                    results[1, TopicCount] = _topics[topicID].Value + " : " + Convert.ToString(DateTime.Now);
+                    results[1, TopicCount] = m_topics[topicID].Value;
                     TopicCount++;
                 }
             }
@@ -84,39 +89,42 @@ namespace QuoteRtd
 
         public int ServerStart(IRTDUpdateEvent CallbackObject)
         {
-            _topics = new Dictionary<int, Topic>();
-            _callback = CallbackObject;
-            _timer = new System.Windows.Forms.Timer();
-            _timer.Tick += Callback;
-            _timer.Interval = 2000;
-            _timer.Start();
+            m_topics = new Dictionary<int, Topic>();
+            m_callback = CallbackObject;
+            m_webClient = new WebClient();
+            m_timer = new Timer();
+            m_timer.Tick += Callback;
+            m_timer.Interval = 2000;
+            m_timer.Start();
 
             return 1;
         }
 
         public void ServerTerminate()
         {
-            _timer.Dispose();
-            _topics = null;
+            m_timer.Dispose();
+            m_topics = null;
         }
 
         #endregion
 
+        //======================================================
+        // Timer callback
+        // Get data from server and update every topic.
+        //======================================================
         private void Callback(object sender, EventArgs e)
         {
 
-            lock (_topics)
+            lock (m_topics)
             {
                 try
                 {
-                    /*
-                     * Multiple topics can use same quotation id
-                     * Put them together in a list and put all lists in a dictionary
-                     * which use quotation id as key
-                     */
+                    // Multiple topics can use same quotation id
+                    // Put them together in a list and put all lists in a dictionary
+                    // which use quotation id as key
                     Dictionary<string, List<Topic>> ids =
                         new Dictionary<string, List<Topic>>();
-                    foreach (KeyValuePair<int, Topic> x in _topics)
+                    foreach (KeyValuePair<int, Topic> x in m_topics)
                     {
                         if (!ids.ContainsKey(x.Value.Id))
                         {
@@ -128,67 +136,125 @@ namespace QuoteRtd
                         ids[x.Value.Id].Add(x.Value);
                     }
 
+                    // Fetch data
                     string[] keys = new string[ids.Keys.Count];
                     ids.Keys.CopyTo(keys, 0);
                     GetDataFromServer(keys);
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
+                    // Show error in logging window
+                    LogDisplay.WriteLine(ex.ToString());
                 }
             }
 
-            _callback.UpdateNotify();
+            m_callback.UpdateNotify();
         }
 
+        //===================================================
+        // Fetch data from sina source
+        //===================================================
         private void GetDataFromServer(string[] ids)
         {
-            string url = "http://hq.sinajs.cn/list=";
+            StringBuilder url = new StringBuilder("http://hq.sinajs.cn/list=");
+
+            // Build the request URL
+            foreach (string id in ids)
+            {
+                url.AppendFormat("{0},", id);
+            }
+            // Remove the last ','
+            url.Remove(url.Length - 1, 1);
+            Debug.Print(url.ToString());
+
+            // Submit request
+            m_webClient.Encoding = Encoding.Default;
+            string result = m_webClient.DownloadString(url.ToString());
+            Debug.Print(result);
+
+            // Parse the result
+            string[] lines = result.Split('\n');
+            Regex regex = new Regex("var hq_str_(.*)=\"(.*?)\"");
+
+            foreach (string line in lines)
+            {
+                // Parse the result line
+                Match match = regex.Match(line);
+
+                // Parse successfully
+                if (match.Groups.Count > 1)
+                {
+                    // Split the result by ','
+                    string[] items = match.Groups[2].Value.Split(',');
+
+                    // Search topics which are interested in this result line
+                    foreach (KeyValuePair<int, Topic> x in m_topics)
+                    {
+                        Topic t = x.Value;
+
+                        // If this line belongs to this topic, update the topic value
+                        if (t.Id == match.Groups[1].Value)
+                        {
+                            // Update value
+                            int idx = Int32.Parse(t.Item);
+                            t.Value = items[idx];
+                        }
+                    }
+                }
+            }
         }
     }
 
+    //========================================
+    // Topic represents a RTD call in Excel
+    //========================================
     public class Topic
     {
-        private string iId;
-        private string iItem;
-        private string iValue;
-        private bool iUpdated;
+        private string m_ID;    // m_ID is the quote id, such as "sh000001"
+        private string m_Item;  // m_Item is a quote item, currently it only supports integer
+                                // e.g. "1" means the second quote item (0-based index)
+        private string m_Value;     // m_Value the quote value
+        private bool m_bUpdated;    // If the topic value has changed
+
+        // Attribute operations
         public string Id
         {
-            get { return iId; }
-            set { iId = value; }
+            get { return m_ID; }
+            set { m_ID = value; }
         }
         public string Item
         {
-            get { return iItem; }
-            set { iItem = value; }
+            get { return m_Item; }
+            set { m_Item = value; }
         }
         public string Value
         {
-            get { return iValue; }
+            get { return m_Value; }
             set
             {
-                if (iValue != value)
+                if (m_Value != value)
                 {
-                    iValue = value;
-                    iUpdated = true;
+                    m_Value = value;
+                    m_bUpdated = true;
                 }
                 else
                 {
-                    iUpdated = false;
+                    m_bUpdated = false;
                 }
             }
         }
         public bool Updated
         {
-            get { return iUpdated; }
-            set { iUpdated = value; }
+            get { return m_bUpdated; }
+            set { m_bUpdated = value; }
         }
 
+        // Constructor
         public Topic(string Id, string Item)
         {
-            iId = Id;
-            iItem = Item;
-            iValue = "";
+            m_ID = Id;
+            m_Item = Item;
+            m_Value = "";
         }
     }
 }
